5c6061438962202bc066ea95eef66467f1c53745
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-21 23:07:57 -0800

    added variables and if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-function pingPong(num) {
+var pingPong = function(userInput) {
+	var range = [];
+    for(var i = 1; i <= userInput; i++) {
 
-	var ping = 3;
-	var pong = 5;
-	var pingpong = 15;
-}
+    	if(i % 15 === 0) {
+    		range.push("pingpong");
+    	}
+    	else if(i % 3 === 0) {
+    		range.push("ping");
+    	}
+    	else if(i % 5 === 0) {
+    		range.push("pong");
+    	}
+    	else {
+    		range.push(i);
+    	}
+    };
+    return range;
+};
